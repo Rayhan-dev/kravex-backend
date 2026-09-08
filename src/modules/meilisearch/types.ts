08c414e600b66ef0d8d587fae203b5e3afda5122
Kamrul Munir Md Rayhan <mohammodrayhan444@gmail.com>
2026-09-09 02:41:47 +0600

    removed search feature

--- src/modules/meilisearch/types.ts
+++ src/modules/meilisearch/types.ts
@@ -1,18 +1,18 @@
-import { SearchTypes } from '@medusajs/types';
-// @ts-ignore
-import type { Config, Settings } from 'meilisearch';
+// import { SearchTypes } from '@medusajs/types';
+// // @ts-ignore
+// import type { Config, Settings } from 'meilisearch';
 
-export interface MeiliSearchPluginOptions {
-  /**
-   * MeiliSearch client configuration
-   */
-  config: Config;
+// export interface MeiliSearchPluginOptions {
+//   /**
+//    * MeiliSearch client configuration
+//    */
+//   config: Config;
 
-  /**
-   * MeiliSearch index settings
-   */
-  settings?: Record<
-    string,
-    SearchTypes.IndexSettings & { indexSettings: Settings }
-  >;
-}
+//   /**
+//    * MeiliSearch index settings
+//    */
+//   settings?: Record<
+//     string,
+//     SearchTypes.IndexSettings & { indexSettings: Settings }
+//   >;
+// }
