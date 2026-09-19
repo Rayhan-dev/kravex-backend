73030dee58751249e38d6ccf10b296bed41a782a
Kamrul Munir Md Rayhan <mohammodrayhan444@gmail.com>
2026-09-19 13:34:26 +0600

    product export xml for meta catelog implemented

--- src/api/middlewares.ts
+++ src/api/middlewares.ts
@@ -1,6 +1,7 @@
 import { defineMiddlewares } from '@medusajs/medusa';
 import { adminProductTypeRoutesMiddlewares } from './store/custom/product-types/middlewares';
-import { authenticate } from '@medusajs/framework';
+import { authenticate, validateAndTransformQuery } from '@medusajs/framework';
+import { z } from '@medusajs/framework/zod';
 
 export default defineMiddlewares([
   ...adminProductTypeRoutesMiddlewares,
@@ -9,4 +10,14 @@ export default defineMiddlewares([
     matcher: '/store/custom/customer/*',
     middlewares: [authenticate('customer', ['session', 'bearer'])],
   },
+  {
+      matcher: "/product-feed",
+      methods: ["GET"],
+      middlewares: [
+        validateAndTransformQuery(z.object({
+          currency_code: z.string(),
+          country_code: z.string(),
+        }), {}),
+      ],
+    },
 ]);
