ef8c47caf54028ecfc7295b652f385fa4090e2d6
Kamrul Munir Md Rayhan <mohammodrayhan444@gmail.com>
2026-09-19 13:48:17 +0600

    product export xml for meta catelog implemented

--- src/api/product-feed/route.ts
+++ src/api/product-feed/route.ts
@@ -1,5 +1,5 @@
 import type { MedusaRequest, MedusaResponse } from "@medusajs/framework/http"
-import generateProductFeedWorkflow from "src/workflows/generate-prodcut-feed"
+import generateProductFeedWorkflow from "src/workflows/generate-product-feed.ts"
 
 export async function GET(
   req: MedusaRequest, 
